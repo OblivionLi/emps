d7cc95ab4b6235d223958be9bc8870b61a539e55
OblivionLi <liviuandrei.dev@gmail.com>
2021-12-14 14:38:38 +0200

    Add PHPMailer const credentials
    => they are used inside the PHPMailer configuration i.e. SendEmail.php class

--- app/Config.php
+++ app/Config.php
@@ -3,9 +3,17 @@
 namespace app;
 
 class Config {
+    // db credentials
     const DB_HOST = 'localhost';
     const DB_NAME = 'emps';
     const DB_USER = 'root';
     const DB_PASS = '';
     const SHOW_ERRORS = true;
+
+    // email credentials
+    const MAIL_HOST = 'smtp.mailtrap.io';
+    const MAIL_PORT = 2525;
+    const MAIL_USER = '5bc86603778757';
+    const MAIL_PASS = '4a0a3ac4538ace';
+    const MAIL_OFFICIAL_EMAIL = 'emps@emps.com';
 }
